2fb1a10832b9d39afad0f24661993c5f3cea317e
accup <32096121+accup@users.noreply.github.com>
2024-03-02 19:10:45 +0900

    refactor:

--- src/styles/reset.css.ts
+++ src/styles/reset.css.ts
@@ -3,35 +3,72 @@ import { recipe } from "@vanilla-extract/recipes";
 import { resetLayer } from "./layer.css";
 
 export const reset = recipe({
-  base: [
-    resetLayer({
-      boxSizing: "border-box",
-      blockSize: "auto",
-      inlineSize: "auto",
-    }),
-    resetLayer({
-      margin: 0,
-      padding: 0,
-    }),
-    resetLayer({
-      display: "block",
-    }),
-    resetLayer({
-      border: "none",
-    }),
-    resetLayer({
-      color: "inherit",
-      background: "none",
-    }),
-    resetLayer({
-      fontSize: "inherit",
-      fontFamily: "inherit",
-      fontWeight: "inherit",
-      lineHeight: "inherit",
-      textDecoration: "none",
-    }),
-    resetLayer({
-      listStyle: "none",
-    }),
-  ],
+  variants: {
+    box: {
+      border: resetLayer({
+        boxSizing: "border-box",
+      }),
+    },
+    size: {
+      auto: resetLayer({
+        minBlockSize: "auto",
+        maxBlockSize: "none",
+        minInlineSize: "auto",
+        maxInlineSize: "none",
+        blockSize: "auto",
+        inlineSize: "auto",
+      }),
+    },
+    margin: {
+      none: resetLayer({
+        margin: 0,
+      }),
+    },
+    padding: {
+      none: resetLayer({
+        padding: 0,
+      }),
+    },
+    display: {
+      block: resetLayer({
+        display: "block",
+      }),
+    },
+    border: {
+      none: resetLayer({
+        border: "none",
+      }),
+    },
+    background: {
+      none: resetLayer({
+        background: "none",
+      }),
+    },
+    text: {
+      inherit: resetLayer({
+        color: "inherit",
+        fontSize: "inherit",
+        fontFamily: "inherit",
+        fontWeight: "inherit",
+        lineHeight: "inherit",
+        textDecoration: "none",
+      }),
+    },
+    list: {
+      none: resetLayer({
+        listStyle: "none",
+      }),
+    },
+  },
+  defaultVariants: {
+    box: "border",
+    size: "auto",
+    margin: "none",
+    padding: "none",
+    display: "block",
+    border: "none",
+    background: "none",
+    text: "inherit",
+    list: "none",
+  },
 });
